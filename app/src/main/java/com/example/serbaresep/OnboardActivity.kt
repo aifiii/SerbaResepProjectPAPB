82709940b762c904da21e6f9f801ac4def754ef8
anantariskys <gamersindo555@gmail.com>
2024-11-23 09:46:36 +0700

    Update OnboardActivity.kt

--- app/src/main/java/com/example/serbaresep/OnboardActivity.kt
+++ app/src/main/java/com/example/serbaresep/OnboardActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 import kotlin.math.log
 
 class OnboardActivity :AppCompatActivity() {
