1ac9b1be252e65340bf045b89ae8dc3b68d622fc
anantariskys <gamersindo555@gmail.com>
2024-11-24 19:02:58 +0700

    Update SupabaseClient.kt

--- app/src/main/java/com/example/serbaresep/supabase/SupabaseClient.kt
+++ app/src/main/java/com/example/serbaresep/supabase/SupabaseClient.kt
@@ -3,12 +3,16 @@ package com.example.supabaseapp
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
+import kotlinx.serialization.serializer
 
 
 val supabase = createSupabaseClient(
     supabaseUrl = "https://exjwylkeissymjyoikya.supabase.co",
     supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4and5bGtlaXNzeW1qeW9pa3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyNTUwNTYsImV4cCI6MjA0NzgzMTA1Nn0.Pb6Go42aTKkjvTC_aRxu9bPt7XCvolT6U7wxer5pTNA"
 ) {
+
     install(Auth) // Untuk autentikasi
     install(Postgrest) // Untuk operasi database
+    install(Storage)
 }
